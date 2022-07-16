264df54a7d4d9f4059d8892daf38574f829a8e47
MarcondesMatheus <matheustkp1@gmail.com>
2022-07-16 13:22:27 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
-import NotificationButton from "./components/NotificationButton"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Header />
       <main> {/*  <!-- Principal, semantica  --> */}
         <section id="sales"> {/* <!-- Uma section  --> */}
